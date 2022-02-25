c53c7be99b5228fd8f9e8ef0bb244bc7a870aadd
Jason Coil <jcoiluni@gmail.com>
2022-02-25 10:26:00 -0500

    Initial Commit - Bare bones server, game objects, game controller, client side html/js, and few simple tests

--- src/main/resources/static/game.js
+++ src/main/resources/static/game.js
@@ -12,6 +12,7 @@ $('document').ready(function(){
     //Set Event Listeners
     document.getElementById("create-game-button").addEventListener("click", createGame);
     document.getElementById("join-game-button").addEventListener("click", connectToSpecificGame);
+    document.getElementById("start-game-button").addEventListener("click", startGame);
 });
 
 //connect to socket function (when user creates game or joins game)
@@ -25,6 +26,10 @@ function connectToSocket(gameId){
             let data = JSON.parse(response.body)
             console.log(data);
             updatePlayerNames(data);
+        });
+        stompClient.subscribe("/user/topic/game-progress/" + gameId, function (response){
+            //let data = JSON.parse(response.body)
+            //console.log(data);
         });
     });
 }
@@ -42,7 +47,7 @@ function createGame(){
             dataType: "json",
             contentType: "application/json",
             data: JSON.stringify({
-                "name": playerName
+                "username": playerName
             }),
             success: function(data){
                 gameId = data.gameId;
@@ -53,6 +58,9 @@ function createGame(){
                 document.getElementById("available-games").style.display = "none";
                 setTimeout(function() {
                     stompClient.send("/topic/game-progress/" + gameId, {}, JSON.stringify(data));
+                    console.log("SENDING PRINCIPAL UPDATE INFO!!!")
+                    stompClient.send("/app/update-principal/" + gameId, {},
+                    JSON.stringify({ "player": {"username": playerName}, "gameId": gameId }));
                 }, 500);
             },
             error: function(error){
@@ -69,7 +77,7 @@ function connectToSpecificGame(){
             alert("please enter name")
         }
         else{
-            let gameId = document.getElementById("join-game").value;
+            gameId = document.getElementById("join-game").value;
             if(gameId == null || gameId === ""){
                         alert("please enter game ID")
             }
@@ -80,7 +88,7 @@ function connectToSpecificGame(){
                 contentType: "application/json",
                 data: JSON.stringify({
                     "player": {
-                        "name": playerName
+                        "username": playerName
                     },
                     "gameId": gameId
                 }),
@@ -95,6 +103,9 @@ function connectToSpecificGame(){
                     document.getElementById("available-games").style.display = "none";
                     setTimeout(function() {
                         stompClient.send("/topic/game-progress/" + gameId, {}, JSON.stringify(data));
+                        console.log("SENDING PRINCIPAL UPDATE INFO!!!")
+                        stompClient.send("/app/update-principal/" + gameId, {},
+                        JSON.stringify({ "player": {"username": playerName}, "gameId": gameId }));
                     }, 500);
                 },
                 error: function(error){
@@ -102,6 +113,11 @@ function connectToSpecificGame(){
                 }
             });
         }
+}
+
+//Start Game
+function startGame(){
+    stompClient.send("/app/play-game/" + gameId, {}, JSON.stringify({ "player": {"name": playerName},"gameId": gameId}));
 }
 
 //Update player names
@@ -112,9 +128,8 @@ function updatePlayerNames(game){
     //Build string of player names
     let playerList = "<h4>Player Names</h4>"
     for(let i = 0; i < players.length; i++){
-        console.log(players[i].name);
-        playerList = playerList + "<p>" + players[i].name + "</p>";
+        console.log(players[i].username);
+        playerList = playerList + "<p>" + players[i].username + "</p>";
     }
-    console.log(playerList);
     document.getElementById("current-player-names").innerHTML = playerList;
 }
