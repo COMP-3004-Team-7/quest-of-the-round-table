d63faaaa348b7eadff8250f72489d5eadcea8e20
Mohumeddahir <khaliddahir722@gmail.com>
2022-02-28 15:06:53 -0500

    Update Card.java

--- src/main/java/comp3004/project/QotRT/Card.java
+++ src/main/java/comp3004/project/QotRT/Card.java
@@ -10,16 +10,6 @@ public class Card {
     int battlepoints;
     int bids;
     String specialevent;
-    ArrayList<String> cards = new ArrayList<String>();
-    ///ArrayList<Card> realcards = new ArrayList<Card>();
-    ArrayList<Player> players = new ArrayList<Player>();
-    String arr[] = {"Horse+10","Sword+10","Dagger+5","Excalibar+30","Lance+20",
-                    "Battle-ax+15","Test of the Questing Beast","Test of temptation",
-                    "Test of Valor","Test of Morghan Le Fey","Robber Knight +15", "Saxons +10/20",
-                    "Boar+5/15","Thieves+5","Green Knight +25/40","Black Knight+25/35","Evil Knight +20/30",
-                    "Saxons Knight+15/25","Dragon+50/70","Giant+40","Mordered+30","Sir Gawain+10",
-                    "King Pellinore+10","Sir Percival+5","Amour+10(1 bid)","Sir Tristan+10",
-                    "King Arthur+10","Queen Guinevere","Merlin","Queen Iseult","Sir Lancelot+15","Sir Galahad+15"};
     public String getName() {
         return name;
 
@@ -36,6 +26,21 @@ public class Card {
     public String getSpecialevent() {
         return specialevent;
     }
+
+
+    public String toString() {
+        StringBuffer display = new StringBuffer();
+        display.append("---- " + name + " ----\n");
+        /*
+        for (Card card : realcards) {
+            display.append(card + "\n");
+        }
+
+         */
+        return display.toString();
+    }
+}
+
 
 
 /*
@@ -55,17 +60,14 @@ public class Card {
         }
 
     }
-
+ArrayList<String> cards = new ArrayList<String>();
+    ///ArrayList<Card> realcards = new ArrayList<Card>();
+   // ArrayList<Player> players = new ArrayList<Player>();
+    String arr[] = {"Horse+10","Sword+10","Dagger+5","Excalibar+30","Lance+20",
+                    "Battle-ax+15","Test of the Questing Beast","Test of temptation",
+                    "Test of Valor","Test of Morghan Le Fey","Robber Knight +15", "Saxons +10/20",
+                    "Boar+5/15","Thieves+5","Green Knight +25/40","Black Knight+25/35","Evil Knight +20/30",
+                    "Saxons Knight+15/25","Dragon+50/70","Giant+40","Mordered+30","Sir Gawain+10",
+                    "King Pellinore+10","Sir Percival+5","Amour+10(1 bid)","Sir Tristan+10",
+                    "King Arthur+10","Queen Guinevere","Merlin","Queen Iseult","Sir Lancelot+15","Sir Galahad+15"
  */
-    public String toString() {
-        StringBuffer display = new StringBuffer();
-        display.append("---- " + name + " ----\n");
-        /*
-        for (Card card : realcards) {
-            display.append(card + "\n");
-        }
-
-         */
-        return display.toString();
-    }
-}
