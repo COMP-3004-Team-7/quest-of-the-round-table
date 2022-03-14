5971c6b99ddad163bb8b286a81187db4a71e4b79
Jason Coil <jcoiluni@gmail.com>
2022-03-14 18:32:39 -0400

    Updated event service to be compatible with the event cards created by Khalid.

--- src/main/java/comp3004/project/QotRT/service/EventService.java
+++ src/main/java/comp3004/project/QotRT/service/EventService.java
@@ -1,7 +1,6 @@
 package comp3004.project.QotRT.service;
 
-import comp3004.project.QotRT.cards.Card;
-import comp3004.project.QotRT.cards.StoryCard;
+import comp3004.project.QotRT.cards.*;
 import comp3004.project.QotRT.model.Game;
 import comp3004.project.QotRT.model.Player;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -11,11 +10,11 @@ import java.util.ArrayList;
 public class EventService {
 
     public void doEvent(Game game, SimpMessagingTemplate simpMessagingTemplate){
-        if(game.getCurrentStoryCard().getName().equals("Chivalrous Deed")){
+        if(game.getCurrentStoryCard() instanceof ChivalrousDeed){
 
         }
         //All players except player drawing this card lose 1 shield
-        else if(game.getCurrentStoryCard().getName().equals("Pox")){
+        else if(game.getCurrentStoryCard() instanceof Pox){
             for(int i = 0; i < game.getPlayers().size(); i++){
                 if(!game.getMainPlayer().getUsername().equals(game.getPlayers().get(i).getUsername())){
                     if(game.getPlayers().get(i).getShields() >= 1){
@@ -26,17 +25,17 @@ public class EventService {
             }
         }
         //Drawer loses 2 shields if possible
-        else if(game.getCurrentStoryCard().getName().equals("Plague")){
+        else if(game.getCurrentStoryCard() instanceof Plague){
             if(game.getMainPlayer().getShields() >= 2){
                 game.getMainPlayer().setShields(game.getMainPlayer().getShields()-2);
                 //Send simp message to player
             }
         }
-        else if(game.getCurrentStoryCard().getName().equals("King's Recognition")){
+        else if(game.getCurrentStoryCard() instanceof KingsRecognition){
 
         }
         //Lowest ranked player(s) draw 2 adventure cards
-        else if(game.getCurrentStoryCard().getName().equals("Queen's Favour")){
+        else if(game.getCurrentStoryCard() instanceof QueensFavor){
             //Get lowest ranked player(s)
             ArrayList<Player> lowestRankedPlayers = new ArrayList<>();
             int lowestRank = 99999;
@@ -60,17 +59,17 @@ public class EventService {
             }
         }
         //All allies in play must be discarded
-        else if(game.getCurrentStoryCard().getName().equals("Court Called to Camelot")){
+        else if(game.getCurrentStoryCard() instanceof CourtCamelot){
             for(int i =0; i < game.getPlayers().size(); i++){
                 game.getPlayers().get(i).setAllies(new ArrayList<>());
                 //Send Simp Message to the player with updated allies (i.e. none)
             }
         }
-        else if(game.getCurrentStoryCard().getName().equals("King's Call to Arms")){
+        else if(game.getCurrentStoryCard() instanceof KingsArms){
 
         }
         //All players draw 2 adventure cards
-        else if(game.getCurrentStoryCard().getName().equals("Prosperity Throughout the Realm")){
+        else if(game.getCurrentStoryCard() instanceof ProsperityRealm){
             for(int i = 0; i < game.getPlayers().size(); i++){
                 Card c1 = game.getAdventureDeck().drawCard();
                 Card c2 = game.getAdventureDeck().drawCard();
